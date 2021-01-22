e7331bf0d54887320bfc5990631366a2bb962b7b
IcE <ivica-matic@outlook.com>
2021-01-22 20:01:39 +0000

    Assigned footprints to the components

--- firmware/build/sketch/ham-radio-dds-board.cpp
+++ firmware/build/sketch/ham-radio-dds-board.cpp
@@ -3,7 +3,7 @@
 HamRadioDDSBoard::HamRadioDDSBoard()
 {
     pinMode(SHIFT_ENABLE, OUTPUT);
-    LiquidCrystal _lcd(PD3, PD4, PD2, PD5, PD6, PD7);
+    LiquidCrystal _lcd(PD3, PB5, PC0, PD5, PD6, PD7);
     _lcd.begin(16, 2);
     this->lcd = &_lcd;
     ShiftRegister74HC595<1> _sr(PD5, PD7, PD6);
